bca705df9263bdcd2ca91d0000a6475e540a1cd7
jjadeseravla <jadealvares@hotmail.co.uk>
2019-06-18 11:07:33 +0100

    moved styles to seperate scss file

--- insta-client/src/App.vue
+++ insta-client/src/App.vue
@@ -5,64 +5,68 @@
 </template>
 
 <style lang="scss">
-  body {
-    margin: 0;
-    padding: 0;
-    background-color: #F3F3F3;
-    font-family: 'Roboto', Helvetica, sans-serif;
-  }
-
-  #app {
-    width: 100vw;
-    min-height: 100vh;
-  }
-
-  .form-group {
-    flex: 1;
-    display: flex;
-    justify-content: flex-start;
-    flex-flow: column;
-    padding: 25px;
-
-    input {
-      width: 100%;
-      height: 30px;
-      border: 1px solid #DDD;
-      margin-bottom: 5px;
-      text-indent: 5px;
-      background: #EEE;
-      outline: none;
-
-      &.err {
-        background: rgba(255, 0, 0, 0.1);
-        border: 1px solid #AF1E2D;
-      }
-
-      &:focus {
-        border: 1px solid #AAA;
-      }
-    }
-
-    button {
-      width: 100%;
-      height: 30px;
-      background: #FFCE00;
-      appearance: none;
-      border: none;
-      outline: none;
-      border-radius: 8px;
-
-      color: #171717;
-      font-size: 15px;
-      font-weight: 700;
-    }
-    .error_msg {
-      margin: 15px 0px;
-      padding: 10px;
-      background-color: rgba(255, 0, 0, 0.2);
-      color: #171717;
-      font-weight: bold;
-      border-radius: 8px;
-    }
-  }
+  @import './assets/scss/main.scss';
 </style>
+
+
+//   body {
+//     margin: 0;
+//     padding: 0;
+//     background-color: #F3F3F3;
+//     font-family: 'Roboto', Helvetica, sans-serif;
+//   }
+//
+//   #app {
+//     width: 100vw;
+//     min-height: 100vh;
+//   }
+//
+//   .form-group {
+//     flex: 1;
+//     display: flex;
+//     justify-content: flex-start;
+//     flex-flow: column;
+//     padding: 25px;
+//
+//     input {
+//       width: 100%;
+//       height: 30px;
+//       border: 1px solid #DDD;
+//       margin-bottom: 5px;
+//       text-indent: 5px;
+//       background: #EEE;
+//       outline: none;
+//
+//       &.err {
+//         background: rgba(255, 0, 0, 0.1);
+//         border: 1px solid #AF1E2D;
+//       }
+//
+//       &:focus {
+//         border: 1px solid #AAA;
+//       }
+//     }
+//
+//     button {
+//       width: 100%;
+//       height: 30px;
+//       background: #FFCE00;
+//       appearance: none;
+//       border: none;
+//       outline: none;
+//       border-radius: 8px;
+//
+//       color: #171717;
+//       font-size: 15px;
+//       font-weight: 700;
+//     }
+//     .error_msg {
+//       margin: 15px 0px;
+//       padding: 10px;
+//       background-color: rgba(255, 0, 0, 0.2);
+//       color: #171717;
+//       font-weight: bold;
+//       border-radius: 8px;
+//     }
+//   }
+// </style>
