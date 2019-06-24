e97bed5b70ca6a50573bda612844c6dbf78c2b78
jjadeseravla <jadealvares@hotmail.co.uk>
2019-06-24 10:48:36 +0100

    sized camera element on new posts page

--- insta-client/src/views/Post.vue
+++ insta-client/src/views/Post.vue
@@ -1,9 +1,13 @@
 <template>
   <main class="view post">
 <section class="stream">
-  <video ref="video" id="video" width="100%" height="300" autoplay>
+  <video ref="video" id="video" width="100%" height="300" autoplay v-if="!captured">
 
   </video>
+  <button @click="capture">Capture</button>
+</section>
+<section class="capture">
+  <canvas ref="canvas" id="canvas" width="100%" height="300" :class="(captured) ? 'show' : 'hide'"></canvas>
 </section>
   </main>
 </template>
@@ -13,14 +17,28 @@
 export default {
   data () {
     return {
-      video: {}
+      video: {},
+      canvas: {},
+      captures: [],
+      hasCaptured: false
     }
   },
-  methods: {},
+  methods: {
+    capture() {
+      let context = this.canvas.getContext('2d').drawImage(this.video, 0, 0, this.canvas.width, this.canvas.height);
+      this.captures.push(canvas.toDataURL("image/png"));
+    }
+  },
   mounted() {
     this.video = this.$refs.video;
     this.video.width = window.innerWidth;
     this.video.height = window.innerHeight - 80;
+
+    if(this.$refs.canvas) {
+    this.canvas = this.$refs.canvas;
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight - 80;
+  }
 
     if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices.getUserMedia({
@@ -36,5 +54,12 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+  .show {
+    display: block;
+  }
+
+  .hide {
+    display: none;
+  }
 </style>
