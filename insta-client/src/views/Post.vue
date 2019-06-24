78024ea6c2c261aecdb160e3101cef0df96dbb04
jjadeseravla <jadealvares@hotmail.co.uk>
2019-06-24 10:39:23 +0100

    enabled camera functionality on /newpost page

--- insta-client/src/views/Post.vue
+++ insta-client/src/views/Post.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="feed">
+  <main class="view post">
 <section class="stream">
   <video ref="video" id="video" width="100%" height="300" autoplay>
 
@@ -18,7 +18,20 @@ export default {
   },
   methods: {},
   mounted() {
+    this.video = this.$refs.video;
+    this.video.width = window.innerWidth;
+    this.video.height = window.innerHeight - 80;
 
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({
+        video: true
+      }).then(stream => {
+        this.video.srcObject = stream;
+        this.video.play();
+      }).catch(err => {
+        console.log(errs);
+      })
+    }
   }
 }
 </script>
